bddfca61149fa87c3df7020ae4bd465f8dc0827c
John Doe <johndoe@email.com>
2023-09-25 23:42:22 +0530

    thumbnail

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-const VideoCard = () => {
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
+
+
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
     return (
-        <div>VideoCard</div>
+        <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
+            <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                <CardMedia
+                    image={snippet?.thumbnails?.high?.url}
+                    alt={snippet?.title}
+                    sx={{ width: { xs: '100%', sm: '358px' }, height: 180 }}
+                />
+            </Link>
+        </Card>
     )
 }
 
